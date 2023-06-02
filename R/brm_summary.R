97cf007c520cb3d2b32109eb20745cc57416560a
wlandau-lilly <will.landau@lilly.com>
2023-06-02 15:39:32 -0400

    Implement #8

--- R/brm_summary.R
+++ R/brm_summary.R
@@ -4,20 +4,33 @@
 #' @description Summarize a basic MMRM model fit.
 #' @details Currently assumes the response variable is `CHG`
 #'   (change from baseline) and not `AVAL` (raw response).
-#' @return A `tibble` with summary statistics of the marginal posterior.
+#' @return A list of two data frames, one with summary statistics on the
+#'   marginal posterior and another with posterior probabilities
+#'   on the treatment effects.
 #' @inheritParams brm_formula
 #' @param model Fitted `brms` model object from [brm_model()].
+#' @param outcome Character of length 1, `"response"` if the
+#'   response variable is the raw outcome variable (such as AVAL)
+#'   or `"change"` if the response variable is change from baseline
+#'   (e.g. CHG).
 #' @param control Element of the `group` column in the data which indicates
 #'   the control group for the purposes of calculating treatment differences.
 #' @param baseline Element of the `time` column in the data
 #'   which indicates the baseline time for the purposes of calculating
-#'   change from baseline. Ignored if `response_type = "change"`.
-#' @param response_type Character of length 1, `"response"` if the
-#'   response variable is the raw outcome variable (such as AVAL)
-#'   or `"change"` if the response variable is change from baseline
-#'   (e.g. CHG).
+#'   change from baseline.
 #' @param level Numeric of length 1 between 0 and 1, credible level
-#'   for the credible intervals in the output.
+#'   for the credible intervals. Only relevant when `return = "marginals"`.
+#' @param direction Character vector of the same length as `threshold`.
+#'   `"greater"` to compute the marginal posterior probability that the
+#'   treatment effect is greater than the threshold,
+#'   `"less"` to compute the marginal posterior probability that the
+#'   treatment effect is less than the threshold.
+#'   Each element `direction[i]` corresponds to `threshold[i]`
+#'   for all `i` from 1 to `length(direction)`.
+#' @param threshold Numeric vector of the same length as `direction`,
+#'   treatment effect threshold for computing posterior probabilities.
+#'   Each element `direction[i]` corresponds to `threshold[i]` for
+#'   all `i` from 1 to `length(direction)`.
 #' @examples
 #' set.seed(0L)
 #' sim <- brm_simulate()
@@ -52,7 +65,7 @@
 #'   patient = "patient",
 #'   control = "treatment 1",
 #'   baseline = "visit 1",
-#'   response_type = "response"
+#'   outcome = "response"
 #' )
 brm_summary <- function(
   model,
@@ -61,22 +74,24 @@ brm_summary <- function(
   time = "AVISIT",
   patient = "USUBJID",
   covariates = character(0),
+  outcome = "change",
   control = "Placebo",
   baseline = "Baseline",
-  response_type = "change",
-  level = 0.95
+  level = 0.95,
+  direction = "greater",
+  threshold = 0
 ) {
   assert_chr(base, "base arg must be a nonempty character string")
   assert_chr(group, "group arg must be a nonempty character string")
   assert_chr(time, "time arg must be a nonempty character string")
   assert_chr(patient, "patient arg must be a nonempty character string")
   assert_chr(
-    response_type,
-    "response_type arg must be a nonempty character string"
+    outcome,
+    "outcome arg must be a nonempty character string"
   )
   assert(
-    response_type %in% c("response", "change"),
-    message = "response_type must be either \"response\" or \"change\""
+    outcome %in% c("response", "change"),
+    message = "outcome must be either \"response\" or \"change\""
   )
   assert_chr_vec(covariates, "covariates arg must be a character vector")
   assert(
@@ -95,6 +110,24 @@ brm_summary <- function(
   )
   assert_num(level, "level arg must be a length-1 numeric between 0 and 1")
   assert(level, . >= 0, . <= 1, message = "level arg must be between 0 and 1")
+  assert(
+    direction,
+    is.character(.),
+    !anyNA(.),
+    nzchar(.),
+    . %in% c("greater", "less"),
+    message = "elements of the direction arg must be \"greater\" or \"less\""
+  )
+  assert(
+    threshold,
+    is.numeric(.),
+    is.finite(.),
+    message = "threshold arg must be a numeric vector"
+  )
+  assert(
+    length(direction) == length(threshold),
+    message = "direction and threshold must have the same length"
+  )
   assert(is.data.frame(model$data))
   data <- model$data
   assert(
@@ -141,8 +174,8 @@ brm_summary <- function(
     replacement = ", ",
     x = colnames(draws_response)
   )
-  groups <- unique(gsub(",.*$", "", colnames(draws_response)))
-  times <- unique(gsub("^.*, ", "", colnames(draws_response)))
+  groups <- unique(group_names(draws_response))
+  times <- unique(time_names(draws_response))
   draws_response[[".chain"]] <- .chain
   draws_response[[".iteration"]] <- .iteration
   draws_response[[".draw"]] <- .draw
@@ -156,7 +189,7 @@ brm_summary <- function(
       paste(groups, collapse = ", ")
     )
   )
-  if (response_type == "response") {
+  if (outcome == "response") {
     assert(
       baseline %in% times,
       message = sprintf(
@@ -166,21 +199,21 @@ brm_summary <- function(
       )
     )
   }
-  if (response_type == "response") {
+  if (outcome == "response") {
     draws_change <- subtract_baseline(
       draws = draws_response,
       groups = groups,
       times = times,
       baseline = baseline
     )
-    draws_diff <- subtract_control(
+    draws_difference <- subtract_control(
       draws = draws_change,
       groups = groups,
       times = setdiff(times, baseline),
       control = control
     )
   } else {
-    draws_diff <- subtract_control(
+    draws_difference <- subtract_control(
       draws = draws_response,
       groups = groups,
       times = times,
@@ -189,17 +222,23 @@ brm_summary <- function(
   }
   table_response <- summarize_marginals(draws_response, level)
   table_change <- if_any(
-    response_type == "response",
+    outcome == "response",
     summarize_marginals(draws_change, level),
     NULL
   )
-  table_diff <- summarize_marginals(draws_diff, level)
-  dplyr::bind_rows(
+  table_difference <- summarize_marginals(draws_difference, level)
+  marginals <- dplyr::bind_rows(
     response = table_response,
     change = table_change,
-    difference = table_diff,
+    difference = table_difference,
     .id = "marginal"
   )
+  probabilities <- summarize_probabilities(
+    draws = draws_difference,
+    direction = direction,
+    threshold = threshold
+  )
+  list(marginals = marginals, probabilities = probabilities)
 }
 
 subtract_baseline <- function(draws, groups, times, baseline) {
@@ -226,10 +265,6 @@ subtract_control <- function(draws, groups, times, control) {
   out
 }
 
-marginal_name <- function(group, time) {
-  sprintf("%s, %s", group , time)
-}
-
 summarize_marginals <- function(draws, level) {
   level_lower <- (1 - level) / 2
   level_upper <- 1 - level_lower
@@ -237,8 +272,8 @@ summarize_marginals <- function(draws, level) {
   draws[[".iteration"]] <- NULL
   draws[[".draw"]] <- NULL
   value <- tibble::tibble(
-    group = gsub(",.*$", "", colnames(draws)),
-    time = gsub("^.*, ", "", colnames(draws)),
+    group = group_names(draws),
+    time = time_names(draws),
     mean = purrr::map_dbl(draws, mean),
     median = purrr::map_dbl(draws, median),
     sd = purrr::map_dbl(draws, sd),
@@ -246,8 +281,8 @@ summarize_marginals <- function(draws, level) {
     upper = purrr::map_dbl(draws, ~quantile(.x, level_upper))
   )
   mcse <- tibble::tibble(
-    group = gsub(",.*$", "", colnames(draws)),
-    time = gsub("^.*, ", "", colnames(draws)),
+    group = group_names(draws),
+    time = time_names(draws),
     mean = purrr::map_dbl(draws, posterior::mcse_mean),
     median = purrr::map_dbl(draws, posterior::mcse_median),
     sd = purrr::map_dbl(draws, posterior::mcse_sd),
@@ -271,4 +306,40 @@ summarize_marginals <- function(draws, level) {
     y = mcse,
     by = c("group", "time", "statistic")
   )
+}
+
+summarize_probabilities <- function(draws, direction, threshold) {
+  draws[[".chain"]] <- NULL
+  draws[[".iteration"]] <- NULL
+  draws[[".draw"]] <- NULL
+  tibble::tibble(
+    group = group_names(draws),
+    time = time_names(draws),
+    direction = direction,
+    theshold = threshold,
+    value = purrr::map_dbl(
+      draws,
+      ~marginal_probability(.x, direction, threshold)
+    )
+  )
+}
+
+marginal_probability <- function(difference, direction, threshold) {
+  if_any(
+    direction == "greater",
+    mean(difference > threshold),
+    mean(difference < threshold)
+  )
+}
+
+marginal_name <- function(group, time) {
+  sprintf("%s, %s", group , time)
+}
+
+group_names <- function(draws) {
+  gsub(",.*$", "", colnames(draws))
+}
+
+time_names <- function(draws) {
+  gsub("^.*, ", "", colnames(draws))
 }
