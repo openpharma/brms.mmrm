b5bf2c7f9d2a2d44e5d60d819d13771ea0351ec1
wlandau-lilly <will.landau@lilly.com>
2023-07-27 15:16:12 -0400

    Fix #28

---
+++ tests/testthat/test-brm_marginal_draws_average.R
@@ -0,0 +1,89 @@
+test_that("brm_marginal_draws_average()", {
+  skip_on_cran()
+  set.seed(0L)
+  data <- brm_data(
+    data = brm_simulate()$data,
+    outcome = "response",
+    role = "response",
+    group = "group",
+    time = "time",
+    patient = "patient"
+  )
+  formula <- brm_formula(
+    data = data,
+    effect_base = FALSE,
+    interaction_base = FALSE
+  )
+  tmp <- utils::capture.output(
+    suppressMessages(
+      suppressWarnings(
+        model <- brm_model(
+          data = data,
+          formula = formula,
+          chains = 1,
+          iter = 100,
+          refresh = 0
+        )
+      )
+    )
+  )
+  out <- brm_marginal_draws(
+    model = model,
+    data = data,
+    control = "group.1",
+    baseline = "time.1"
+  )
+  averages_all <- brm_marginal_draws_average(
+    draws = out,
+    data = data
+  )
+  averages_sub <- brm_marginal_draws_average(
+    draws = out,
+    data = data,
+    times = c("time 2", "time 3")
+  )
+  for (group in setdiff(unique(data$group), "group.1")) {
+    cols_all <- grep(
+      pattern = group,
+      x = colnames(out$response),
+      fixed = TRUE,
+      value = TRUE
+    )
+    cols_sub <- grep(
+      pattern = "time\\.2|time\\.3",
+      x = cols_all,
+      fixed = FALSE,
+      value = TRUE
+    )
+    expect_equal(
+      averages_all$response[[group]],
+      apply(tibble::as_tibble(out$response)[, cols_all], 1, mean)
+    )
+    expect_equal(
+      averages_sub$response[[group]],
+      apply(tibble::as_tibble(out$response)[, cols_sub], 1, mean)
+    )
+    cols_all <- intersect(cols_all, colnames(out$change))
+    cols_sub <- intersect(cols_sub, colnames(out$change))
+    expect_equal(
+      averages_all$change[[group]],
+      apply(tibble::as_tibble(out$change)[, cols_all], 1, mean)
+    )
+    expect_equal(
+      averages_sub$change[[group]],
+      apply(tibble::as_tibble(out$change)[, cols_sub], 1, mean)
+    )
+    cols_all <- intersect(cols_all, colnames(out$difference))
+    cols_sub <- intersect(cols_sub, colnames(out$difference))
+    for (field in c("difference", "effect")) {
+      expect_equal(
+        averages_all[[field]][[group]],
+        apply(tibble::as_tibble(out[[field]])[, cols_all], 1, mean)
+      )
+      expect_equal(
+        averages_sub[[field]][[group]],
+        apply(tibble::as_tibble(out[[field]])[, cols_sub], 1, mean)
+      )
+    }
+  }
+})
