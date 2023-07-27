3f91e4a97ac798bcf15c07d0afa9c3765c8c333b
wlandau-lilly <will.landau@lilly.com>
2023-07-27 15:27:11 -0400

    compat with existing summary functions

--- R/brm_marginal_draws_average.R
+++ R/brm_marginal_draws_average.R
@@ -1,10 +1,11 @@
 #' @title Average marginal MCMC draws across time points.
 #' @export
 #' @family marginals
-#' @description Average marginal MCMC draws across time points.
+#' @description Simple un-weighted arithmetic mean of
+#'   marginal MCMC draws across time points.
 #' @inheritSection brm_data Separation string
 #' @return A named list of tibbles of MCMC draws of the marginal posterior
-#'   distribution of each treatment group:
+#'   distribution of each treatment group.
 #'   * `response`: on the scale of the response variable.
 #'   * `change`: change from baseline, where the `baseline` argument determines
 #'     the time point at baseline. Only returned if the `role` argument is
@@ -13,7 +14,8 @@
 #'   * `difference`: treatment effect of change from baseline, where the
 #'     `control` argument identifies the placebo or active control group.
 #'   In each tibble, there is 1 row per posterior sample and one column for
-#'   each treatment group.
+#'   each treatment group. There is a time point label which is called
+#'   `"average"` by default.
 #' @inheritParams brm_marginal_draws
 #' @param draws Fitted `brms` model object from [brm_model()].
 #' @param times Character vector of discrete time point levels
@@ -21,6 +23,10 @@
 #'   Set to `NULL` to average across all time points. Levels are automatically
 #'   sanitized with `make.names(unique = FALSE, allow_ = TRUE)` to ensure
 #'   agreement with `brms` variable names in downstream computations.
+#' @param label Character of length 1, time point label for the averages.
+#'   Automatically sanitized with `make.names(unique = FALSE, allow_ = TRUE)`.
+#'   Must not conflict with any existing time point labels in the data
+#'   after the label and time points are sanitized.
 #' @examples
 #' if (identical(Sys.getenv("BRM_EXAMPLES", unset = ""), "true")) {
 #' set.seed(0L)
@@ -60,10 +66,16 @@
 #' brm_marginal_draws_average(
 #'   draws = draws,
 #'   data = data,
-#'   times = c("time 1", "time 2")
+#'   times = c("time 1", "time 2"),
+#'   label = "mean"
 #' )
 #' }
-brm_marginal_draws_average <- function(draws, data, times = NULL) {
+brm_marginal_draws_average <- function(
+  draws,
+  data,
+  times = NULL,
+  label = "average"
+) {
   assert(
     is.list(draws),
     message = "marginals arg must be a named list from brm_marginal_draws()"
@@ -81,25 +93,45 @@ brm_marginal_draws_average <- function(draws, data, times = NULL) {
     all(. %in% attr(data, "brm_levels_time")),
     message = "times argument must be valid discrete time points from the data"
   )
+  label <- brm_levels(label)
+  assert(
+    label,
+    is.character(.),
+    !anyNA(.),
+    nzchar(.),
+    length(.) == 1L,
+    !any(. %in% attr(data, "brm_levels_time")),
+    message = paste(
+      "label must be a string and must not conflict",
+      "with existing time point labels."
+    )
+  )
   levels_time <- brm_levels(levels_time)
   for (field in names(draws)) {
     draws[[field]] <- brm_marginal_draws_average_df(
       draws = draws[[field]],
       levels_group = levels_group,
-      levels_time = levels_time
+      levels_time = levels_time,
+      label = label
     )
   }
   draws
 }
 
-brm_marginal_draws_average_df <- function(draws, levels_group, levels_time) {
+brm_marginal_draws_average_df <- function(
+  draws,
+  levels_group,
+  levels_time,
+  label
+) {
   original_columns <- setdiff(colnames(draws), names_mcmc)
   for (group in levels_group) {
     names <- name_marginal(group, levels_time)
     names <- intersect(names, colnames(draws))
     if (length(names) > 0L) {
       subset <- tibble::as_tibble(draws)[, names, drop = FALSE]
-      draws[[group]] <- apply(X = subset, MARGIN = 1L, FUN = mean)
+      name <- name_marginal(group, label)
+      draws[[name]] <- apply(X = subset, MARGIN = 1L, FUN = mean)
     }
   }
   draws[, setdiff(colnames(draws), original_columns), .drop = FALSE]
--- tests/testthat/test-brm_marginal_draws_average.R
+++ tests/testthat/test-brm_marginal_draws_average.R
@@ -35,12 +35,14 @@ test_that("brm_marginal_draws_average()", {
   )
   averages_all <- brm_marginal_draws_average(
     draws = out,
-    data = data
+    data = data,
+    label = "average"
   )
   averages_sub <- brm_marginal_draws_average(
     draws = out,
     data = data,
-    times = c("time 2", "time 3")
+    times = c("time 2", "time 3"),
+    label = "mean"
   )
   for (group in setdiff(unique(data$group), "group.1")) {
     cols_all <- grep(
@@ -56,34 +58,38 @@ test_that("brm_marginal_draws_average()", {
       value = TRUE
     )
     expect_equal(
-      averages_all$response[[group]],
+      averages_all$response[[name_marginal(group, "average")]],
       apply(tibble::as_tibble(out$response)[, cols_all], 1, mean)
     )
     expect_equal(
-      averages_sub$response[[group]],
+      averages_sub$response[[name_marginal(group, "mean")]],
       apply(tibble::as_tibble(out$response)[, cols_sub], 1, mean)
     )
     cols_all <- intersect(cols_all, colnames(out$change))
     cols_sub <- intersect(cols_sub, colnames(out$change))
     expect_equal(
-      averages_all$change[[group]],
+      averages_all$change[[name_marginal(group, "average")]],
       apply(tibble::as_tibble(out$change)[, cols_all], 1, mean)
     )
     expect_equal(
-      averages_sub$change[[group]],
+      averages_sub$change[[name_marginal(group, "mean")]],
       apply(tibble::as_tibble(out$change)[, cols_sub], 1, mean)
     )
     cols_all <- intersect(cols_all, colnames(out$difference))
     cols_sub <- intersect(cols_sub, colnames(out$difference))
     for (field in c("difference", "effect")) {
       expect_equal(
-        averages_all[[field]][[group]],
+        averages_all[[field]][[name_marginal(group, "average")]],
         apply(tibble::as_tibble(out[[field]])[, cols_all], 1, mean)
       )
       expect_equal(
-        averages_sub[[field]][[group]],
+        averages_sub[[field]][[name_marginal(group, "mean")]],
         apply(tibble::as_tibble(out[[field]])[, cols_sub], 1, mean)
       )
     }
   }
+  expect_true(is.list(suppressWarnings(brm_marginal_summaries(averages_all))))
+  expect_true(is.list(suppressWarnings(brm_marginal_summaries(averages_sub))))
+  expect_true(is.list(brm_marginal_probabilities(averages_all)))
+  expect_true(is.list(brm_marginal_probabilities(averages_sub)))
 })
