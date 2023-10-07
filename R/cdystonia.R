a103fd7f979071878f5a24ecc7331e59954fb8bf
Stock,Dr.,Christian (MED BDS) BIP-DE-I <christian.stock@boehringer-ingelheim.com>
2023-10-07 17:58:03 +0200

    Include modified cdystonia dataset in package

---
+++ R/cdystonia.R
@@ -0,0 +1,24 @@
+#' cdystonia dataset
+#'
+#' Subset from a multicenter, randomized controlled trial of botulinum toxin 
+#' type B in patients with cervical dystonia.
+#'
+#' @format A tibble with 420 rows and 6 variables
+#' 
+#' \describe{
+#'   \item id. ...
+#'   \item sex. ...
+#'   \item age. ...
+#'   \item baseline. ...   
+#'   \item treat, ...
+#'   \item week. ...
+#'   \item chg ...
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name cdystonia
+#' @usage data(cdystonia)
+
+#' @references \url{https://hbiostat.org/data/repo/cdystonia}
+"cdystonia"
---
+++ data-raw/cdystonia.R
@@ -0,0 +1,29 @@
+Hmisc::getHdata(cdystonia)
+cdystonia <- cdystonia |>
+  dplyr::filter(treat %in% c("10000U", "Placebo")) |> 
+  droplevels() |>
+  dplyr::mutate_all(~ {
+    attr(., "label") <- NULL
+    .
+  }) |>
+  dplyr::mutate(
+    week = paste0("w", sprintf("%02d", week)),
+    id = paste0(site, "-", sprintf("%02d", id))
+  ) |>
+  dplyr::select(-site) |>
+  tibble::as_tibble() |>
+  brms.mmrm::brm_data(
+    outcome = "twstrs",
+    role = "response",
+    group = "treat",
+    time = "week",
+    patient = "id",
+    covariates = c("age", "sex"),
+    level_control = "Placebo",
+    level_baseline = "w00"
+    ) |>
+  brms.mmrm::brm_data_change(name_change = "chg") |> 
+  dplyr::filter(!is.na(chg)) |>
+  dplyr::select(id, sex, age, baseline, treat, week, chg) |>
+  dplyr::arrange(id, week)
+usethis::use_data(cdystonia, overwrite = TRUE)
