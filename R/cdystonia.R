f7217663fb441d5d151268bfb1f82ed452d90e38
Stock,Dr.,Christian (MED BDS) BIP-DE-I <christian.stock@boehringer-ingelheim.com>
2023-10-08 14:54:36 +0200

    Updates (parts of the preprocessing of the dataset now excluded to be done in the vignette)

--- R/cdystonia.R
+++ R/cdystonia.R
@@ -1,17 +1,28 @@
 #' cdystonia dataset
 #'
-#' Subset from a multicenter, randomized controlled trial of botulinum toxin
-#' type B in patients with cervical dystonia.
+#' Subset of data from a multicenter, randomized controlled trial of
+#' botulinum toxin type B (BotB) in patients with cervical dystonia.
+#' The endpoint of the trial was the total score on the Toronto Western
+#' Spasmodic Torticollis Rating Scale (TWSTRS). The dataset contains data from
+#' 72 patients and is limited to 2 (of originally 3) treatment arms.
 #'
-#' @format A tibble with 347 rows and 7 variables
+#' @format A tibble with 420 rows and 6 variables
 #'
 #' \describe{
-#'   \item{id}{id}
-#'   \item{sex}{sex}
-#'   \item{age}{age}
-#'   \item{baseline}{baseline}
-#'   \item{treat}{treat}
-#'   \item{week}{week}
-#'   \item{chg}{chg}
+#'   \item{id}{Character. Unique subject identifier}
+#'   \item{age}{Integer.  Age of subject in years}
+#'   \item{sex}{Character.  Sex of subject (`F`, female; `M`, male)}
+#'   \item{treat}{Character. Treatment (`Placebo`, placebo, `X10000U`,
+#'   10,000 units of BotB)}
+#'   \item{week}{Character. Week of study visit}
+#'   \item{twstrs}{Integer. Total TWSTRS score}
 #' }
+#' @source The data were drawn from the \pkg{Hmisc} package.
+#' For further details see
+#' \url{https://hbiostat.org/data/repo/cdystonia}.
+#' @usage data(cdystonia)
+#' @keywords datasets
+#' @examples
+#' data(cdystonia)
+#' head(cdystonia)
 "cdystonia"
--- data-raw/cdystonia.R
+++ data-raw/cdystonia.R
@@ -2,28 +2,18 @@ Hmisc::getHdata(cdystonia)
 cdystonia <- cdystonia |>
   dplyr::filter(treat %in% c("10000U", "Placebo")) |>
   droplevels() |>
-  dplyr::mutate_all(~ {
-    attr(., "label") <- NULL
-    .
-  }) |>
+  dplyr::mutate_all(
+    ~ {
+      attr(., "label") <- NULL
+      .
+    }
+  ) |>
   dplyr::mutate(
+    sex = as.character(sex),
     week = paste0("w", sprintf("%02d", week)),
     id = paste0(site, "-", sprintf("%02d", id))
   ) |>
-  dplyr::select(-site) |>
-  tibble::as_tibble() |>
-  brms.mmrm::brm_data(
-    outcome = "twstrs",
-    role = "response",
-    group = "treat",
-    time = "week",
-    patient = "id",
-    covariates = c("age", "sex"),
-    level_control = "Placebo",
-    level_baseline = "w00"
-  ) |>
-  brms.mmrm::brm_data_change(name_change = "chg") |>
-  dplyr::filter(!is.na(chg)) |>
-  dplyr::select(id, sex, age, baseline, treat, week, chg) |>
-  dplyr::arrange(id, week)
+  dplyr::select(id, age, sex, treat, week, twstrs) |>
+  dplyr::arrange(id, week) |>
+  tibble::as_tibble()
 usethis::use_data(cdystonia, overwrite = TRUE)
