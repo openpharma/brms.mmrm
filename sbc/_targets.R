bf419b19c504611691df45b9a8d9c81b435ee4d4
wlandau-lilly <will.landau@lilly.com>
2023-10-18 15:39:59 -0400

    Scale back up

--- sbc/_targets.R
+++ sbc/_targets.R
@@ -8,26 +8,26 @@ tar_option_set(
   memory = "transient",
   garbage_collection = TRUE,
   error = "null",
-  workspace_on_error = TRUE
-  # controller = crew_controller_aws_batch(
-  #   name = "brms-mmrm-sbc",
-  #   workers = 25L,
-  #   seconds_idle = 120,
-  #   seconds_launch = 1800,
-  #   launch_max = 3L,
-  #   processes = 4,
-  #   aws_batch_job_definition = Sys.getenv("JOB_DEFINITION", unset = "job"),
-  #   aws_batch_job_queue = Sys.getenv("JOB_QUEUE", unset = "queue")
-  # ),
-  # repository = "aws",
-  # cue = tar_cue(file = FALSE),
-  # resources = tar_resources(
-  # aws = tar_resources_aws(
-  #   bucket = Sys.getenv("BUCKET", unset = "bucket"),
-  #   prefix = Sys.getenv("PREFIX", unset = "prefix"),
-  #   region = Sys.getenv("REGION", unset = "region")
-  # )
-# )
+  workspace_on_error = TRUE,
+  controller = crew_controller_aws_batch(
+    name = "brms-mmrm-sbc",
+    workers = 25L,
+    seconds_idle = 120,
+    seconds_launch = 1800,
+    launch_max = 3L,
+    processes = 4,
+    aws_batch_job_definition = Sys.getenv("JOB_DEFINITION", unset = "job"),
+    aws_batch_job_queue = Sys.getenv("JOB_QUEUE", unset = "queue")
+  ),
+  repository = "aws",
+  cue = tar_cue(file = FALSE),
+  resources = tar_resources(
+    aws = tar_resources_aws(
+      bucket = Sys.getenv("BUCKET", unset = "bucket"),
+      prefix = Sys.getenv("PREFIX", unset = "prefix"),
+      region = Sys.getenv("REGION", unset = "region")
+    )
+  )
 )
 
 tar_source()
@@ -41,13 +41,13 @@ list(
       prior = prior_simple,
       chains = 1,
       warmup = 20,
-      iter = 40
-      # chains = 4L,
-      # warmup = 2000L,
-      # iter = 4000L
-    )
-    # batches = 100,
-    # reps = 10
+      iter = 40,
+      chains = 4L,
+      warmup = 2000L,
+      iter = 4000L
+    ),
+    batches = 100,
+    reps = 10
   ),
   tar_target(simple, ranks_simple),
   tar_rep(
@@ -56,13 +56,13 @@ list(
       prior = prior_complex,
       chains = 1,
       warmup = 20,
-      iter = 40
-      # chains = 4L,
-      # warmup = 2000L,
-      # iter = 4000L
-    )
-    # batches = 100,
-    # reps = 10
+      iter = 40,
+      chains = 4L,
+      warmup = 2000L,
+      iter = 4000L
+    ),
+    batches = 100,
+    reps = 10
   ),
   tar_target(complex, ranks_complex)
 )
