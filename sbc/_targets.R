e2e5f77dcec15f5eb54b6d6da599e14c1cee8286
wlandau <will.landau@lilly.com>
2023-12-14 10:20:18 -0500

    cloud

--- sbc/_targets.R
+++ sbc/_targets.R
@@ -9,33 +9,33 @@ tar_option_set(
   format = "qs",
   garbage_collection = TRUE,
   workspace_on_error = TRUE,
-  controller = crew_controller_sge(
-    name = "brms-mmrm-sbc",
-    workers = 100L,
-    seconds_idle = 120,
-    seconds_launch = 1800,
-    launch_max = 3L,
-    script_lines = "module load R/4.2.2",
-    sge_cores = 4L
-  )
-  # controller = crew_controller_aws_batch(
+  # controller = crew_controller_sge(
   #   name = "brms-mmrm-sbc",
-  #   workers = 50L,
+  #   workers = 100L,
   #   seconds_idle = 120,
   #   seconds_launch = 1800,
   #   launch_max = 3L,
-  #   processes = 4,
-  #   aws_batch_job_definition = Sys.getenv("JOB_DEFINITION", unset = "job"),
-  #   aws_batch_job_queue = Sys.getenv("JOB_QUEUE", unset = "queue")
-  # ),
-  # repository = "aws",
-  # resources = tar_resources(
-  #   aws = tar_resources_aws(
-  #     bucket = Sys.getenv("BUCKET", unset = "bucket"),
-  #     prefix = Sys.getenv("PREFIX", unset = "prefix"),
-  #     region = Sys.getenv("REGION", unset = "region")
-  #   )
+  #   script_lines = "module load R/4.2.2",
+  #   sge_cores = 4L
   # )
+  controller = crew_controller_aws_batch(
+    name = "brms-mmrm-sbc",
+    workers = 50L,
+    seconds_idle = 120,
+    seconds_launch = 1800,
+    launch_max = 3L,
+    processes = 4,
+    aws_batch_job_definition = Sys.getenv("JOB_DEFINITION", unset = "job"),
+    aws_batch_job_queue = Sys.getenv("JOB_QUEUE", unset = "queue")
+  ),
+  repository = "aws",
+  resources = tar_resources(
+    aws = tar_resources_aws(
+      bucket = Sys.getenv("BUCKET", unset = "bucket"),
+      prefix = Sys.getenv("PREFIX", unset = "prefix"),
+      region = Sys.getenv("REGION", unset = "region")
+    )
+  )
 )
 
 tar_source()
