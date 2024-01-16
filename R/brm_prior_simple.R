00ecd006068d8eea541826132edce64e3ff1ef75
wlandau <will.landau.oss@gmail.com>
2024-01-16 10:40:24 -0500

    testing and docs for brm_prior_simple()

--- R/brm_prior_simple.R
+++ R/brm_prior_simple.R
@@ -10,6 +10,18 @@
 #'   to different fixed effect parameters.
 #' @return A classed data frame with the `brms` prior.
 #' @inheritParams brm_model
+#' @param intercept Character of length 1, Stan code for the prior
+#'   to set on the intercept parameter.
+#' @param coefficients Character of length 1, Stan code for the prior
+#'   to set independently on each of the non-intercept model coefficients.
+#' @param sigma Character of length 1, Stan code for the prior
+#'   to set independently on each of the log-scale standard deviation
+#'   parameters. Should be a symmetric prior in most situations.
+#' @param correlation Character of length 1, Stan code for the prior
+#'   on the correlation matrix for the residuals of a given patient.
+#'   (Different patients are modeled as independent, and each
+#'   patient has the same correlation structure as each other patient.)
+#'   Should be an LKJ prior in most situations.
 #' @examples
 #' set.seed(0L)
 #' data <- brm_simulate_outline()
@@ -19,7 +31,14 @@
 #'   baseline = FALSE,
 #'   baseline_time = FALSE
 #' )
-#' brm_prior_simple(data = data, formula = formula)
+#' brm_prior_simple(
+#'   data = data,
+#'   formula = formula,
+#'   intercept = "student_t(3, 0, 2.5)",
+#'   coefficients = "normal(0, 10)",
+#'   sigma = "student_t(2, 0, 4)",
+#'   correlation = "lkj(2.5)"
+#' )
 brm_prior_simple <- function(
   data,
   formula,
@@ -32,6 +51,22 @@ brm_prior_simple <- function(
   assert(
     inherits(formula, "brmsformula"),
     message = "formula arg must be a \"brmsformula\" object."
+  )
+  assert_chr(
+    intercept,
+    message = "'intercept' must be a valid character string"
+  )
+  assert_chr(
+    coefficients,
+    message = "'coefficients' must be a valid character string"
+  )
+  assert_chr(
+    sigma,
+    message = "'sigma' must be a valid character string"
+  )
+  assert_chr(
+    correlation,
+    message = "'correlation' must be a valid character string"
   )
   data[[attr(data, "brm_outcome")]] <- 0
   prior <- brms::get_prior(formula = formula, data = data)
