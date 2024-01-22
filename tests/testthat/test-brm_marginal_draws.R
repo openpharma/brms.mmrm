451277f7a81e599c892f3eb1b9c9c106cc3c5652
wlandau <will.landau.oss@gmail.com>
2024-01-22 11:40:26 -0500

    Add a subgroup draws test

--- tests/testthat/test-brm_marginal_draws.R
+++ tests/testthat/test-brm_marginal_draws.R
@@ -113,6 +113,152 @@ test_that("brm_marginal_draws() on response, no subgroup", {
   }
 })
 
+test_that("brm_marginal_draws() on response, with subgroup", {
+  skip_on_cran()
+  set.seed(0L)
+  data <- brm_simulate_outline(
+    n_group = 2L,
+    n_subgroup = 2L,
+    n_patient = 25L,
+    n_time = 4L
+  )
+  data$response <- rnorm(n = nrow(data))
+  formula <- brm_formula(
+    data = data,
+    baseline = FALSE,
+    baseline_time = FALSE
+  )
+  tmp <- utils::capture.output(
+    suppressMessages(
+      suppressWarnings(
+        model <- brm_model(
+          data = data,
+          formula = formula,
+          chains = 1,
+          iter = 100,
+          refresh = 0
+        )
+      )
+    )
+  )
+  old_sep <- emmeans::get_emm_option("sep")
+  out <- brm_marginal_draws(
+    model = model,
+    data = data,
+    use_subgroup = TRUE
+  )
+  expect_equal(emmeans::get_emm_option("sep"), old_sep)
+  fields <- c(
+    "response",
+    "difference_time",
+    "difference_group",
+    "difference_subgroup",
+    "effect"
+  )
+  columns_df <- expand.grid(
+    group = sort(unique(data$group)),
+    subgroup = sort(unique(data$subgroup)),
+    time = sort(unique(data$time)),
+    stringsAsFactors = FALSE
+  )
+  columns <- paste(
+    columns_df$group,
+    columns_df$subgroup,
+    columns_df$time,
+    sep = brm_sep()
+  )
+  expect_equal(sort(names(out)), sort(fields))
+  for (field in fields) {
+    x <- out[[field]]
+    expect_true(tibble::is_tibble(x))
+    expect_true(all(colnames(x) %in% c(columns, names_mcmc)))
+    expect_false(any(unlist(lapply(x, anyNA))))
+    expect_equal(nrow(x), 50)
+  }
+  expect_equal(
+    sort(colnames(out$response)),
+    sort(c(columns, names_mcmc))
+  )
+  columns_df <- columns_df[columns_df$time != "time_1", ]
+  columns <- paste(
+    columns_df$group,
+    columns_df$subgroup,
+    columns_df$time,
+    sep = brm_sep()
+  )
+  expect_equal(
+    sort(colnames(out$difference_time)),
+    sort(c(columns, names_mcmc))
+  )
+  columns_df <- columns_df[columns_df$group != "group_1", ]
+  columns <- paste(
+    columns_df$group,
+    columns_df$subgroup,
+    columns_df$time,
+    sep = brm_sep()
+  )
+  expect_equal(
+    sort(colnames(out$difference_group)),
+    sort(c(columns, names_mcmc))
+  )
+  columns_df <- columns_df[columns_df$subgroup != "subgroup_1", ]
+  columns <- paste(
+    columns_df$group,
+    columns_df$subgroup,
+    columns_df$time,
+    sep = brm_sep()
+  )
+  expect_equal(
+    sort(colnames(out$difference_subgroup)),
+    sort(c(columns, names_mcmc))
+  )
+  draws <- tibble::as_tibble(posterior::as_draws_df(model))
+  draws <- draws[, grep("^b_sigma_", colnames(draws), value = TRUE)]
+  colnames(draws) <- gsub("^b_sigma_", "", colnames(draws))
+  colnames(draws) <- gsub(paste0("^time"), "", x = colnames(draws))
+  sigma <- exp(draws)
+  for (group in setdiff(unique(data$group), "group_1")) {
+    for (subgroup in setdiff(unique(data$subgroup), "subgroup_1")) {
+      for (time in setdiff(unique(data$time), "time_1")) {
+        name1 <- paste(group, "subgroup_1", time, sep = brm_sep())
+        name2 <- paste(group, subgroup, time, sep = brm_sep())
+        expect_equal(
+          out$difference_subgroup[[name2]],
+          out$difference_group[[name2]] - out$difference_group[[name1]]
+        )
+      }
+    }
+  }
+  for (group in setdiff(unique(data$group), "group_1")) {
+    for (subgroup in unique(data$subgroup)) {
+      for (time in setdiff(unique(data$time), "time_1")) {
+        name1 <- paste("group_1", subgroup, time, sep = brm_sep())
+        name2 <- paste(group, subgroup, time, sep = brm_sep())
+        expect_equal(
+          out$difference_group[[name2]],
+          out$difference_time[[name2]] - out$difference_time[[name1]]
+        )
+        expect_equal(
+          out$effect[[name2]],
+          out$difference_group[[name2]] / sigma[[time]]
+        )
+      }
+    }
+  }
+  for (group in unique(data$group)) {
+    for (subgroup in unique(data$subgroup)) {
+      for (time in setdiff(unique(data$time), "time_1")) {
+        name1 <- paste(group, subgroup, "time_1", sep = brm_sep())
+        name2 <- paste(group, subgroup, time, sep = brm_sep())
+        expect_equal(
+          out$difference_time[[name2]],
+          out$response[[name2]] - out$response[[name1]]
+        )
+      }
+    }
+  }
+})
+
 test_that("brm_marginal_draws() on change, no subgroup", {
   skip_on_cran()
   set.seed(0L)
