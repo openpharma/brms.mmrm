78a4ffd69972342be0dc87c440f23d675d16c7b9
wlandau <will.landau.oss@gmail.com>
2024-07-15 13:22:55 -0400

    Fix #121

--- tests/testthat/test-brm_model.R
+++ tests/testthat/test-brm_model.R
@@ -75,3 +75,41 @@ test_that("brm_model() runs, impute missing values during model", {
   draws <- posterior::as_draws_df(model)
   expect_true("ymi[1]" %in% tolower(colnames(draws)))
 })
+
+test_that("brm_model() runs, impute missing values before model", {
+  skip_on_cran()
+  set.seed(0L)
+  data <- tibble::as_tibble(brm_simulate_simple()$data)
+  data$response[1L] <- NA_real_
+  data <- brm_data(
+    data = data,
+    outcome = "response",
+    group = "group",
+    time = "time",
+    patient = "patient",
+    reference_group = "group_1",
+    reference_time = "time_1"
+  )
+  imputed <- suppressWarnings(mice::mice(data, m = 2L, print = FALSE))
+  formula <- brm_formula(
+    data = data,
+    model_missing_outcomes = FALSE,
+    baseline = FALSE,
+    baseline_time = FALSE
+  )
+  tmp <- utils::capture.output(
+    suppressMessages(
+      suppressWarnings(
+        model <- brm_model(
+          data = data,
+          formula = formula,
+          chains = 1,
+          iter = 100,
+          refresh = 0
+        )
+      )
+    )
+  )
+  expect_s3_class(model, "brmsfit")
+  expect_true(is.list(brm_marginal_draws(model = model)))
+})
