1964cd691b61fbc32963e42bd15ca94284d61ad9
wlandau <will.landau.oss@gmail.com>
2024-07-29 13:05:23 -0400

    sketch sdif clda

--- R/brm_archetype_successive_cells.R
+++ R/brm_archetype_successive_cells.R
@@ -169,8 +169,8 @@ brm_archetype_successive_cells <- function(
   )
   archetype <- if_any(
     brm_data_has_subgroup(data),
-    archetype_successive_cells_subgroup(data, prefix_interest),
-    archetype_successive_cells(data, prefix_interest)
+    archetype_successive_cells_subgroup(data, clda, prefix_interest),
+    archetype_successive_cells(data, clda, prefix_interest)
   )
   brm_archetype_init(
     data = data,
@@ -183,15 +183,16 @@ brm_archetype_successive_cells <- function(
     baseline_subgroup_time = baseline_subgroup_time,
     baseline_time = baseline_time,
     covariates = covariates,
-    clda = clda,
     prefix_nuisance = prefix_nuisance,
     subclass = "brms_mmrm_successive_cells"
   )
 }
 
-archetype_successive_cells <- function(data, prefix) {
+archetype_successive_cells <- function(data, clda, prefix) {
   group <- attr(data, "brm_group")
   time <- attr(data, "brm_time")
+  reference_group <- attr(data, "brm_reference_group")
+  reference_time <- attr(data, "brm_reference_time")
   levels_group <- brm_levels(data[[group]])
   levels_time <- brm_levels(data[[time]])
   n_time <- length(levels_time)
@@ -215,13 +216,28 @@ archetype_successive_cells <- function(data, prefix) {
     time = names_time,
     variable = names
   )
+  if (clda) {
+    index_keep <- mapping$group == reference_group &
+      mapping$time == reference_time
+    keep <- mapping$variable[index_keep]
+    for (name_group in setdiff(levels_group, reference_group)) {
+      index_drop <- mapping$group == name_group &
+        mapping$time == reference_time
+      drop <- mapping$variable[index_drop]
+      interest[[keep]] <- as.integer(interest[[keep]] | interest[[drop]])
+      interest[[drop]] <- NULL
+      mapping <- mapping[mapping$variable != drop,, drop = FALSE] # nolint
+    }
+  }
   list(interest = interest, mapping = mapping)
 }
 
-archetype_successive_cells_subgroup <- function(data, prefix) {
+archetype_successive_cells_subgroup <- function(data, clda, prefix) {
   group <- attr(data, "brm_group")
   subgroup <- attr(data, "brm_subgroup")
   time <- attr(data, "brm_time")
+  reference_group <- attr(data, "brm_reference_group")
+  reference_time <- attr(data, "brm_reference_time")
   levels_group <- brm_levels(data[[group]])
   levels_subgroup <- brm_levels(data[[subgroup]])
   levels_time <- brm_levels(data[[time]])
@@ -254,5 +270,22 @@ archetype_successive_cells_subgroup <- function(data, prefix) {
     time = names_time,
     variable = names
   )
+  if (clda) {
+    for (name_subgroup in levels_subgroup) {
+      index_keep <- mapping$group == reference_group &
+        mapping$subgroup == name_subgroup &
+        mapping$time == reference_time
+      keep <- mapping$variable[index_keep]
+      for (name_group in setdiff(levels_group, reference_group)) {
+        index_drop <- mapping$group == name_group &
+          mapping$subgroup == name_subgroup &
+          mapping$time == reference_time
+        drop <- mapping$variable[index_drop]
+        interest[[keep]] <- as.integer(interest[[keep]] | interest[[drop]])
+        interest[[drop]] <- NULL
+        mapping <- mapping[mapping$variable != drop,, drop = FALSE] # nolint
+      }
+    }
+  }
   list(interest = interest, mapping = mapping)
 }
